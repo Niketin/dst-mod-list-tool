858ed2908400a8cf73cc61ea5a3a176fcab5e448
Niketin <Niketin@users.noreply.github.com>
2021-03-09 20:54:24 +0200

    Add command-line options parser

--- mod_list_creator.py
+++ mod_list_creator.py
@@ -1,10 +1,11 @@
-import sys
 import os
+import argparse
 
-input_path = sys.argv[1]
-output_path = "dedicated_server_mods_setup.lua"
+default_output_path = "dedicated_server_mods_setup.lua"
+description = f"""Mod list generator for Don't Starve Together Dedicated Server.
+  By default it outputs the file '{default_output_path} to the current directory.'
+"""
 
-print(input_path)
 
 def get_mod_id_from_path(mod_path):
     dir_name = os.path.basename(mod_path)
@@ -39,7 +40,7 @@ def get_mod_name_and_id(mod_path):
 
 
 def write_dedicated_server_mods_setup_lua(mods, output_path):
-    with open(output_path, "w") as f:
+    with open(output_path, "a") as f:
         for mod_path, name in mods:
             id = get_mod_id_from_path(mod_path)
             f.write(f"--#{name}\n")
@@ -48,5 +49,32 @@ def write_dedicated_server_mods_setup_lua(mods, output_path):
     print(f"File '{file_name}' created succesfully!")
 
 
-l = sorted(list(get_all_mods(input_path)))
-write_dedicated_server_mods_setup_lua(l, output_path)
+def main():
+    parser = argparse.ArgumentParser(
+        description=description, formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument(
+        "mods_path", help="Path to Don't Starve Together mods folder")
+    parser.add_argument(
+        "--output-path", help="Override the output file path. A new file is automatically created. If the file already exists, the result will be appended to the end of the file.")
+    args = parser.parse_args()
+
+    output_path = default_output_path
+    if args.output_path:
+        output_path = args.output_path
+
+    mods_path = None
+    if args.mods_path:
+        mods_path = args.mods_path
+    else:
+        # TODO find mods path automatically. 'mods_path' needs to be optional argument.
+        pass
+
+    mods = sorted(list(get_all_mods(mods_path)))
+    print(f"Generated {len(mods)} items.")
+    write_dedicated_server_mods_setup_lua(mods, output_path)
+
+
+
+if __name__ == "__main__":
+    main()
